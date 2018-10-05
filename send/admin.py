de59ec4ccdad14573cee4a080e4429acc0558b7d
iftixor <iftixor.rajabov@gmail.com>
2018-10-05 10:43:38 +0500

    first commit

--- send/admin.py
+++ send/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Upload, UploadImage
 
-#
-# class SendFileAdmin(admin.ModelAdmin):
-#     exclude = ('owner',)
 
-admin.site.register(Upload)
-admin.site.register(UploadImage)
+class UploadImageInline(admin.StackedInline):
+    model = UploadImage
+
+
+class UploadAdmin(admin.ModelAdmin):
+    inlines = [
+        UploadImageInline,
+    ]
+
+
+admin.site.register(Upload, UploadAdmin)
